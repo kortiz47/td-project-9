ba3ffb20bbc4c6e3c09fa8e665c865e96fb24198
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-22 12:33:43 -0800

    /api/users POST route created and sending back 201 status and location / header

--- routes.js
+++ routes.js
@@ -16,12 +16,35 @@ function asyncHandler(cb) {
     }
 }
 //==================================ROUTES=====================================
-
+/** GET all properties and values for the currently authenticated User */
 router.get('/users', asyncHandler(async (req, res) => {
-    //res.send('route works')
     let user = await User.findAll();
-    console.log(user);
-    res.send(user);
+    res.json(user);
 }));
+
+router.post('/users', asyncHandler(async(req, res)=>{
+    let user;
+    try{
+        user =  await User.build(req.body);
+        res.redirect(201, '/');
+    }catch(error){
+        const errorMsgs = error.errors.map(error => error.message);
+        res.send(errorMsgs);
+    }
+}));
+// router.post('/users', asyncHandler(async(req, res)=>{
+//     console.log(req.body);
+//     try{
+//         let user;
+//         user = await User.create(req.body);
+//         res.status(201).end();
+//     }catch(error){
+//         res.send(error);
+//     }
+    
+    
+// }));
+
+//==============================EXPORTS=======================================
 
 module.exports = router;
