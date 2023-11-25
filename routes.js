c4996dbd55d163a5253c684ce0b4160e45c482b4
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-24 19:09:49 -0800

    validation to post and put requests in user and course routes added

--- routes.js
+++ routes.js
@@ -1,15 +1,17 @@
 //PART OF EXPRESS SETUP
 const express = require('express');
 const router = express.Router();
-const User = require("./models").User;
-const Course = require("./models").Course;
-const { Op } = require("sequelize");
-//body parser
-const bodyParser = require('body-parser');
-// parse application/x-www-form-urlencoded
-router.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-router.use(bodyParser.json());
+const User = require('./models').User;
+const Course = require('./models').Course;
+const { Op } = require('sequelize');
+
+router.use(express.json());
+// //body parser
+// const bodyParser = require('body-parser');
+// // parse application/x-www-form-urlencoded
+// router.use(bodyParser.urlencoded({ extended: false }));
+// // parse application/json
+// router.use(bodyParser.json());
 
 //=============================TRY/CATCH HANDLER=============================
 function asyncHandler(cb) {
@@ -21,6 +23,8 @@ function asyncHandler(cb) {
         }
     }
 }
+
+
 //==================================USER routes=====================================
 
 /** GET all properties and values for the currently authenticated User */
@@ -29,29 +33,51 @@ router.get('/users', asyncHandler(async (req, res) => {
     res.json(user);
 }));
 
+
+
 /** POST route to create new user and location header to / */
+//come back and add try/catch if you need it
 router.post('/users', asyncHandler(async (req, res) => {
-    console.log(req.body);
+    const errors = [];
     let user;
     try {
-        user = await User.create(req.body);
-        res.status(201).location('/').end();
+        user = await User.build(req.body);
+
+        if (!user.firstName || '') {
+            errors.push("Please provide a value for 'first name'");
+        }
+        if (!user.lastName || '') {
+            errors.push("Please provide a value for 'last name'");
+        }
+        if (!user.emailAddress || '') {
+            errors.push("Please provide a value for 'email address'");
+        }
+        if (!user.password || '') {
+            errors.push("Please provide a value for 'password'");
+        }
+
+        if (errors.length > 0) {
+            res.status(400).json({ errors }).end();
+        } else {
+            await user.save();
+            res.status(201).location('/').end();
+        }
     } catch (error) {
-        const errorMsgs = error.errors.map(error => error.message);
-        res.status(500).json(errorMsgs);
+        const errors = error.errors.map(error => error.message);
+        res.status(500).json(errors);
     }
-    //TODO
-    
-    //USER validations - all 400 errors
-    /**
-     * include validation to make sure the following required values are properly submitted
-     * firstName
-     * if(!user.firstName) - send a validation error - EXTRA: could also use regex in the User model under firstName to add more validation
-     * if(!user.lastName) - same as firstName
-     * if(!user.emailAddress) - same and in the model change STRING to isEmail
-     * if(!user.password) - if there needs to be constraints on the password check for that
-     * also add hashing to the password
-     */
+
+}));
+
+router.delete('/users/:id', asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    if (user) {
+        await user.destroy();
+        res.status(204).end();
+    } else {
+        res.status(404).end();
+    }
 }));
 
 //==================================COURSE routes====================================
@@ -61,6 +87,8 @@ router.get('/courses', asyncHandler(async (req, res) => {
     res.json(course);
 }));
 
+
+
 /** GET a specific course and userId associated with it */
 router.get('/courses/:id', asyncHandler(async (req, res) => {
     const id = req.params.id;
@@ -68,67 +96,88 @@ router.get('/courses/:id', asyncHandler(async (req, res) => {
     res.json(course);
 }))
 
+
+
 /** POST a new course to /courses */
 router.post('/courses', asyncHandler(async (req, res) => {
+    const errors = [];
     let course;
     try {
-        course = await Course.create(req.body);
-        console.log(course);
-        //res.json(course);
-        res.status(201).location(`/courses/${course.id}'`).end();
-    } catch (error) {
-        const errorMsgs = error.errors.map(error => error.message);
-        res.status(500).json(errorMsgs);
-    }
-    //TO DO
+        course = await Course.build(req.body);
 
-    //COURSE validations
-    /**
-     * if(!course.title) - send error 400
-     * if(!course.description) - send error 400
-     */
+        if (!course.title || '') {
+            errors.push("Please provide a value for 'title'");
+        }
+        if (!course.description || '') {
+            errors.push("Please provide a value for 'description'");
+        }
+
+        if (errors.length > 0) {
+            res.status(400).json({ errors }).end();
+        } else {
+            await course.save();
+            res.status(201).location(`/courses/${course.id}'`).end();
+        }
+    } catch (error) {
+        const errors = error.errors.map(error => error.message);
+        res.status(500).json(errors);
+    }
+
 }))
+
+
 
 /** PUT (edit) an existing course */
 router.put('/courses/:id', asyncHandler(async (req, res) => {
-    let course;
-    try {
-        const id = req.params.id;
-        course = await Course.findByPk(id);
-    //COURSE validations
-    /**
-     * if(!course.title) - send error 400
-     * if(!course.description) - send error 400
-     * else you can send the correct status
-     */
-        if(course){
-            course.update(req.body);
-            res.status(204).end();
-        } else {
-            res.status(404).end();
+    const errors = [];
+    const id = req.params.id;
+    const course = await Course.findByPk(id);
+
+    if (course) {
+        await course.set({
+            title: req.body.title,
+            description: req.body.description,
+            estimatedTime: req.body.estimatedTime,
+            materialsNeeded: req.body.materialsNeeded,
+            userId: req.body.userId
+        })
+
+        if (!course.title) {
+            errors.push("Please provide a value for 'title'");
         }
-    } catch (error) {
-        const errorMsgs = error.errors.map(error => error.message);
-        res.status(500).json(errorMsgs);
+        if (!course.description) {
+            errors.push("Please provide a value for 'description'");
+        }
+
+        if (errors.length > 0) {
+            res.status(400).json({ errors }).end();
+        } else {
+            await course.save();
+            res.status(204).end();
+        }
+
+    } else {
+        res.status(404).end();
     }
+
+
 }));
+
+
 
 /** DELETE an existing course */
-router.delete('/courses/:id', asyncHandler(async(req, res)=>{
+router.delete('/courses/:id', asyncHandler(async (req, res) => {
     const id = req.params.id;
-    let course;
-    try{
-        course = await Course.findByPk(id);
-        if(course){
-            await course.destroy();
-            res.status(204).end();
-        }else{
-            res.status(404).end();
-        }
-    }catch(error){
-
+    const course = await Course.findByPk(id);
+    if (course) {
+        await course.destroy();
+        res.status(204).end();
+    } else {
+        res.status(404).end();
     }
 }));
+
+
 //==============================EXPORTS=======================================
 
 module.exports = router;
